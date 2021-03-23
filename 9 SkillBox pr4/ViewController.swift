edc2ef123630af024880edc38567df609815481b
Kirill12a <kirilldrozd12@gmail.com>
2021-03-24 00:19:08 +0300

    Initial Commit

---
+++ 9 SkillBox pr4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9 SkillBox pr4
+//
+//  Created by Kirill Drozdov on 24.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
